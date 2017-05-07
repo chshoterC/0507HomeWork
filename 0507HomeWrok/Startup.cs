5158d55e8cead023d931fc23761c213055224768
Cloud <Cloud@DESKTOP-6IK53K1>
2017-05-07 17:22:38 +0800

    加入專案檔案。

---
+++ 0507HomeWrok/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_0507HomeWrok.Startup))]
+namespace _0507HomeWrok
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
