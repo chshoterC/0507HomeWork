878e314fbba04f0042bac86eef6b786dd320a482
Cloud <driftscloud@gmail.com>
2017-05-12 14:34:03 +0800

    基本CRUD雛形

---
+++ 0507HomeWrok/Controllers/客戶聯絡人Controller.cs
@@ -0,0 +1,124 @@
+﻿using _0507HomeWrok.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0507HomeWrok.Controllers
+{
+    public class 客戶聯絡人Controller : Controller
+    {
+        ////// GET: 客戶銀行資訊
+        客戶資料Entities db = new 客戶資料Entities();
+        public ActionResult Index(string str類型, string str查詢值)
+        {
+            var all = db.客戶聯絡人.AsQueryable();
+            var data = all.Where(p => p.是否刪除 == false).OrderBy(p => p.Id);
+            if (str類型 != null && str查詢值 != null)
+            {
+                switch (str類型)
+                {
+                    case "客戶名Id":
+                        data = all.Where(p => p.客戶Id == Convert.ToInt32(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "職稱":
+                        data = all.Where(p => p.職稱.Contains(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "姓名":
+                        data = all.Where(p => p.姓名.Contains(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "Email":
+                        data = all.Where(p => p.Email.Contains(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "手機":
+                        data = all.Where(p => p.手機.Contains(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "電話":
+                        data = all.Where(p => p.電話.Contains(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                }
+            }
+            return View(data);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(客戶聯絡人 客戶聯絡人Item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶聯絡人.Add(客戶聯絡人Item);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id != null)
+            {
+                var data = db.客戶聯絡人.Find(id);
+                return View(data);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.客戶聯絡人.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, 客戶聯絡人 客戶聯絡人Item)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.客戶聯絡人.Find(id);
+
+                item.職稱 = 客戶聯絡人Item.職稱;
+                item.姓名 = 客戶聯絡人Item.姓名;
+                item.Email = 客戶聯絡人Item.Email;
+                item.手機 = 客戶聯絡人Item.手機;
+                item.電話 = 客戶聯絡人Item.電話;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var 客戶聯絡人Item = db.客戶聯絡人.Find(id);
+
+            客戶聯絡人Item.是否刪除 = true;
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 0507HomeWrok/Controllers/客戶銀行資訊Controller.cs
@@ -0,0 +1,116 @@
+﻿using _0507HomeWrok.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0507HomeWrok.Controllers
+{
+    public class 客戶銀行資訊Controller : Controller
+    {
+        //// GET: 客戶銀行資訊
+        客戶資料Entities db = new 客戶資料Entities();
+        public ActionResult Index(string str類型, string str查詢值)
+        {
+            var all = db.客戶銀行資訊.AsQueryable();
+            var data = all.Where(p => p.是否刪除 == false).OrderBy(p => p.Id);
+            if (str類型 != null && str查詢值 != null)
+            {
+                switch (str類型)
+                {
+                    case "客戶名Id":
+                        data = all.Where(p => p.客戶Id == Convert.ToInt32(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "銀行代碼":
+                        data = all.Where(p => p.銀行代碼 == Convert.ToInt32(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "銀行名稱":
+                        data = all.Where(p => p.銀行名稱.Contains(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                    case "帳戶號碼":
+                        data = all.Where(p => p.帳戶號碼.Contains(str查詢值) && p.是否刪除 == false)
+                        .OrderByDescending(p => p.Id);
+                        break;
+                }
+            }
+            return View(data);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(客戶銀行資訊 客戶銀行資訊Item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶銀行資訊.Add(客戶銀行資訊Item);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id != null)
+            {
+                var data = db.客戶銀行資訊.Find(id);
+                return View(data);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.客戶銀行資訊.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, 客戶銀行資訊 客戶銀行資訊Item)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.客戶銀行資訊.Find(id);
+
+                item.帳戶名稱 = 客戶銀行資訊Item.帳戶名稱;
+                item.帳戶號碼 = 客戶銀行資訊Item.帳戶號碼;
+                item.分行代碼 = 客戶銀行資訊Item.分行代碼;
+                item.銀行代碼 = 客戶銀行資訊Item.銀行代碼;
+                item.銀行名稱 = 客戶銀行資訊Item.銀行名稱;
+              
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var 客戶銀行資訊Item = db.客戶銀行資訊.Find(id);
+
+            客戶銀行資訊Item.是否刪除 = true;
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
